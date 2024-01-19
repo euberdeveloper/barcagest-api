3afbdef6f5c7d0b1910f6d926b6d1a36fdbf6063
euberdeveloper <euberdeveloper@gmail.com>
2024-01-19 21:30:05 +0100

    feat: use cors
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import config from './common/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.server.port);
 }
