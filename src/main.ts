c51593415f15eea50ae0a6e58d7eeaf3c8ff9a54
euberdeveloper <euberdeveloper@gmail.com>
2024-01-19 21:30:50 +0100

    feat: add helmet
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 import config from './common/config';
 
@@ -8,6 +9,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   await app.listen(config.server.port);
 }
 bootstrap();
